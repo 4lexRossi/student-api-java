ab11a7ec3b564e9ab84a04c10ee35f0e94530b2a
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-08-24 21:06:21 -0300

    initial spring project

---
+++ src/main/java/com/alos/fpt/apifaculpratodos/ApiFaculPraTodosApplication.java
@@ -0,0 +1,13 @@
+package com.alos.fpt.apifaculpratodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFaculPraTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFaculPraTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alos/fpt/apifaculpratodos/ApiFaculPraTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alos.fpt.apifaculpratodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFaculPraTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
