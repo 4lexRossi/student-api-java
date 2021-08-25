4187727bba78789e9b6458e66c294493358ace41
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-08-24 22:57:10 -0300

    created get method and services

--- src/main/java/com/alos/fpt/apifaculpratodos/services/StudentService.java
+++ src/main/java/com/alos/fpt/apifaculpratodos/services/StudentService.java
@@ -1,12 +1,17 @@
-package com.alos.fpt.apifaculpratodos.controller.services;
+package com.alos.fpt.apifaculpratodos.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.alos.fpt.apifaculpratodos.dto.request.StudentDTO;
 import com.alos.fpt.apifaculpratodos.entities.Student;
+import com.alos.fpt.apifaculpratodos.dto.mapper.StudentMapper;
 import com.alos.fpt.apifaculpratodos.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class StudentService {
